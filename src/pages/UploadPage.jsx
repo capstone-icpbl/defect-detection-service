b249bb5179e79069d78d3356b4060d5618d640f0
wphee01 <wphee01@mju.ac.kr>
2025-11-24 06:04:21 +0900

    수정

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -16,13 +16,11 @@ const UploadPage = ({ projectData, fetchHistory, onUploadSuccess, projectInterna
             
             {/* 2. 이미지 업로드 영역 (공통 컴포넌트로 교체하여 로직 통일) */}
             <div style={{ marginTop: '30px', width: '100%', display: 'flex', justifyContent: 'center' }}>
-                <div style={{ width: '90%', maxWidth: '800px' }}>
-                    <ImageUploadArea 
-                        projectId={projectInternalId}
-                        fetchHistory={fetchHistory}
-                        onUploadSuccess={onUploadSuccess}
-                    />
-                </div>
+                <ImageUploadArea 
+                    projectId={projectInternalId}
+                    fetchHistory={fetchHistory}
+                    onUploadSuccess={onUploadSuccess}
+                />
             </div>
             
             {/* 3. 하단 히스토리 목록 */}
